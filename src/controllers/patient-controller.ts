eaf34be86c0af2a3a928c7893a619a51ef24c6ce
Silas Augusto <augusto.fernandes775@gmail.com>
2023-04-01 16:32:29 -0300

    feat: implement get all patient controller

--- src/controllers/patient-controller.ts
+++ src/controllers/patient-controller.ts
@@ -61,4 +61,22 @@ export class PatientControllers {
       return response.status(500).json({ message: 'internal server error' })
     }
   }
+
+  findAll = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const { limit, page } = request.query
+
+      const patients = await this.patientServices.findAll({ limit: Number(limit ?? 10), page: Number(page ?? 0) })
+
+      return response.status(200).send(patients)
+
+    } catch (error) {
+      console.log(error)
+
+      if (error instanceof AppError)
+        return response.status(error.statusCode).json({ message: error.message })
+
+      return response.status(500).json({ message: 'internal server error' })
+    }
+  }
 }
