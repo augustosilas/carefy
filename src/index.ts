20b5b90374b3881509f4ef26ed927783d556022d
Silas Augusto <augusto.fernandes775@gmail.com>
2023-04-03 13:11:49 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 import { createConnection } from 'typeorm'
 import { pgConfig } from './infra/pg-config'
 import { patientRouter } from './routes'
@@ -9,6 +10,7 @@ const PORT = process.env.PORT
 
 createConnection(pgConfig)
   .then(_ => {
+    app.use(cors())
     app.use(express.json())
     app.use(patientRouter)
   })
